e41ab26329d88ee727b494a3e0ca1064ae40da24
Akshayp001 <akshay.patil21@pccoepune.org>
2024-01-29 14:53:50 +0530

    firsttempCOmming

---
+++ server.js
@@ -0,0 +1,19 @@
+const express =require('express');
+
+const app=express();
+app.use(express.json());
+
+app.post('/temp', async (req, res) => {
+    try {
+        const { var1, var2 } = req.body;
+        const result = var1 + var2;
+        return res.json({ result });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error/Invalid Input' });
+    }
+});
+
+app.listen(3000,()=>{
+    console.log("Server Connected On Post 3000");
+});
